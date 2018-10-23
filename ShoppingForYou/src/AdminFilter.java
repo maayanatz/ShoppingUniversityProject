75f177b02430795db68d690892fa5b943c7a91a1
maayanatz <maayanatz@gmail.com>
2018-10-24 01:19:18 +0300

    Working Admin Login

--- ShoppingForYou/src/AdminFilter.java
+++ ShoppingForYou/src/AdminFilter.java
@@ -29,27 +29,11 @@ public class AdminFilter implements Filter {
 			HttpServletRequest reqt = (HttpServletRequest) request;
 			HttpServletResponse resp = (HttpServletResponse) response;
 			HttpSession ses = reqt.getSession(false);
-
-			String reqURI = reqt.getRequestURI();
-			if (reqURI.indexOf("/login-admin.xhtml") >= 0
-					|| (ses != null && ses.getAttribute("currentEmail") != null)
-					|| reqURI.indexOf("/publicArea/") >= 0
-					|| reqURI.contains("javax.faces.resource"))
-				{
-				chain.doFilter(request, response);
 			
-				System.out.println("====================================================================");
-				System.out.println(chain.toString());
-				System.out.println(reqt.getContextPath());
-				System.out.println(ses.getAttribute("currentEmail"));
-				System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-				}
-			else {
-				System.out.println("====================================================================");
-				System.out.println(reqt.getContextPath());
-				System.out.println("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-				resp.sendRedirect(reqt.getContextPath() + "/publicArea/login-admin.xhtml");
-			}
+			if (ses != null && ses.getAttribute("currentEmail") != null)
+				chain.doFilter(request, response);
+			else
+				resp.sendRedirect(reqt.getContextPath() + "/faces/login-admin.xhtml");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
